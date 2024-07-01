0884d934540b7ddb051caaaa79c76fd85d760c77
Jacob Stover <jwstover@gmail.com>
2024-07-01 17:27:48 -0400

    add toggle input type

--- lib/kindling_ui/components/input.ex
+++ lib/kindling_ui/components/input.ex
@@ -41,7 +41,7 @@ defmodule KindlingUI.Components.Input do
   attr :type, :string,
     default: "text",
     values: ~w(checkbox color date datetime-local email file month number password
-               range search select tel text textarea time url week)
+               range search select tel text textarea time toggle url week)
 
   attr :field, Phoenix.HTML.FormField,
     doc: "a form field struct retrieved from the form, for example: @form[:email]"
