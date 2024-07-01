6170b5aa2882ef361e2a13c24a006c5c6d08a445
Jacob Stover <jwstover@gmail.com>
2024-07-01 17:33:12 -0400

    remove IO.inspect()

--- lib/kindling_ui/components/input.ex
+++ lib/kindling_ui/components/input.ex
@@ -71,7 +71,6 @@ defmodule KindlingUI.Components.Input do
     assigns =
       assign_new(assigns, :checked, fn ->
         Phoenix.HTML.Form.normalize_value("checkbox", assigns[:value])
-        |> IO.inspect()
       end)
 
     ~H"""
